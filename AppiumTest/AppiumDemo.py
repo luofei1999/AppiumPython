e3aec3a8364646be0aabfc432e5f05f0e02a8f69
luofei1999 <37337637+luofei1999@users.noreply.github.com>
2019-07-19 15:59:08 +0800

    添加了注释

--- AppiumTest/AppiumDemo.py
+++ AppiumTest/AppiumDemo.py
@@ -1,20 +1,20 @@
 #!usr/bin/python3
-#coding:utf-8
+
 import time
-import os
-
+# 导入 Python 需要使用的 webdriver
 from appium import webdriver
-desired_caps = {}
-desired_caps['platformName'] = 'Android'
-desired_caps['deviceName'] = 'a0b021697d13'
-desired_caps['platformVersion'] = '8.1.0'
-desired_caps['appPackage'] = 'com.android.calculator2'
-desired_caps['appActivity'] = 'com.android.calculator2.Calculator'
-
+# 设备信息
+desired_caps = {'platformName': 'Android', 'deviceName': 'a0b021697d13', 'platformVersion': '8.1.0',
+                'appPackage': 'com.android.calculator2', 'appActivity': 'com.android.calculator2.Calculator'}
+# 初始化设备
 driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
-
+# 延时执行，太快的话会导致定位不到控件位置
 time.sleep(5)
+# 1
 driver.find_element_by_id("com.google.android.calculator:id/digit_1").click()
+# +
 driver.find_element_by_id("com.google.android.calculator:id/op_add").click()
+# 8
 driver.find_element_by_id("com.google.android.calculator:id/digit_8").click()
+# =
 driver.find_element_by_id("com.google.android.calculator:id/eq").click()
