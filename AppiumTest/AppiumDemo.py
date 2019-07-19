0def916fd854f23aa07158d45a17beb0a7889a43
luofei1999 <37337637+luofei1999@users.noreply.github.com>
2019-07-19 16:58:05 +0800

    对动作封装

--- AppiumTest/AppiumDemo.py
+++ AppiumTest/AppiumDemo.py
@@ -3,18 +3,35 @@
 import time
 # 导入 Python 需要使用的 webdriver
 from appium import webdriver
+
 # 设备信息
+
 desired_caps = {'platformName': 'Android', 'deviceName': 'a0b021697d13', 'platformVersion': '8.1.0',
                 'appPackage': 'com.android.calculator2', 'appActivity': 'com.android.calculator2.Calculator'}
 # 初始化设备
 driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
 # 延时执行，太快的话会导致定位不到控件位置
 time.sleep(5)
+# 点击行为
+click_actions = ["com.google.android.calculator:id/digit_1", "com.google.android.calculator:id/op_add",
+                 "com.google.android.calculator:id/digit_8", "com.google.android.calculator:id/eq"]
+
+
+# 行为的具体操作函数
+def test_action(actions):
+    for action in actions:
+        driver.find_element_by_id(action).click()
+
+
+# 调用操作函数，并将动作传入
+test_action(click_actions)
+
+# test_action(actions)
 # 1
-driver.find_element_by_id("com.google.android.calculator:id/digit_1").click()
+# driver.find_element_by_id("com.google.android.calculator:id/digit_1").click()
 # +
-driver.find_element_by_id("com.google.android.calculator:id/op_add").click()
+# driver.find_element_by_id("com.google.android.calculator:id/op_add").click()
 # 8
-driver.find_element_by_id("com.google.android.calculator:id/digit_8").click()
+# driver.find_element_by_id("com.google.android.calculator:id/digit_8").click()
 # =
-driver.find_element_by_id("com.google.android.calculator:id/eq").click()
+# driver.find_element_by_id("com.google.android.calculator:id/eq").click()
---
+++ AppiumTest/actionDemo.py
@@ -0,0 +1,6 @@
+#!usr/bin/python3
+
+# def test_action(driver, *actions):
+#     for index in range(len(actions)):
+#         print(actions[index])
+#         driver.find_element_by_id(actions[index]).click()
